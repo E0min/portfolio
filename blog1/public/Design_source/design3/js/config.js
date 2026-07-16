b3ddc71625fae970839dba30439c55e1b1c09a1c
이영민 <bbok4yo@gmail.com>
2026-07-16 15:14:44 +0900

    레거시 블로그 포트폴리오 파일 정리

--- blog1/public/Design_source/design3/js/config.js
+++
@@ -1,1 +0,0 @@
-const apiKey = 'a19507f0d4606dad9b1a296fef393c4d';
--- blog1/public/Design_source/design5/next.config.mjs
+++
@@ -1,5 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
-};
-export default nextConfig;
